98db534196b4b40a15a3ed75a1d92a339300980e
Madhav <madhavarao.mrt@gmail.com>
2025-01-06 22:50:56 +0530

    User implemented

--- src/main/java/com/tmr/springbootdemo/controller/HomeController.java
+++ src/main/java/com/tmr/springbootdemo/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.tmr.springbootdemo.controller;
 
+import com.tmr.springbootdemo.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@ResponseBody
+@RestController
 public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "Hello World!";
+    }
+
+    //@RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setId("333");
+        user.setName("Madhava");
+        user.setEmail("madhavarao.mrt@gmail.com");
+        return user;
     }
 }
